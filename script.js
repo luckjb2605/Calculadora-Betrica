b6d07d8267effffad28054e9eb2d51b6231f7ffd
Lucas Jalles <luckjb2605@gmail.com>
2026-06-21 14:20:57 -0300

    Fita Bétrica v1.0

---
+++ script.js
@@ -0,0 +1,112 @@
+const inputNumber = document.getElementById("input");
+
+const sourceUnit = document.getElementById("sourceUnit");
+const sourceRelatedBoxes = document.querySelectorAll(".sourceUnit");
+
+const targetUnit = document.getElementById("targetUnit");
+const targetRelatedBoxes = document.querySelectorAll(".targetUnit");
+
+const submitBtn = document.querySelector(".submit");
+const outputText = document.querySelector(".output");
+
+const conversionSheet =
+{
+  centimetro :
+  {
+    centibetro : 1 / 1.92,
+    metro : 1 / 100,
+    betro : 1 / 192
+  },
+  centibetro :
+  {
+    centimetro : 1.92,
+    metro : 1.92 / 100,
+    betro : 1 / 100
+  },
+  metro :
+  {
+    centimetro : 100,
+    centibetro : 100 / 1.92,
+    betro : 1 / 1.92
+  },
+  betro :
+  {
+    centimetro : 192,
+    centibetro : 100,
+    metro : 1.92
+  },
+}
+
+function ResetState()
+{
+  inputNumber.classList.remove('error');
+  sourceRelatedBoxes.forEach(b => b.classList.remove('error'));
+  targetRelatedBoxes.forEach(b => b.classList.remove('error'));
+  outputText.classList.remove('error');
+  outputText.textContent = "Sua conversão aparecerá aqui!";
+}
+
+function ErrorNoMeasureToConvert()
+{
+  inputNumber.classList.add('error');
+  outputText.classList.add('error');
+  outputText.textContent
+    = "Insira um número para a conversão!";
+}
+
+function ErrorNoSourceUnitSelected()
+{
+  sourceRelatedBoxes.forEach((box) => {
+    box.classList.add('error');
+  })
+
+  outputText.classList.add('error');
+  outputText.textContent
+    = "Selecione uma unidade para converter de!";
+}
+
+function ErrorNoTargetUnitSelected()
+{
+  targetRelatedBoxes.forEach((box) => {
+    box.classList.add('error');
+  })
+
+  outputText.classList.add('error');
+  outputText.textContent
+    = "Selecione uma unidade para converter para!";
+}
+
+submitBtn.addEventListener('click', () => {
+
+  ResetState();
+
+  let numToBeConverted, startingUnit, endUnit;
+
+  if ((numToBeConverted = inputNumber.value) === '')
+  {
+    ErrorNoMeasureToConvert();
+    return;
+  }
+
+  if ((startingUnit = sourceUnit.value) === '')
+  {
+    ErrorNoSourceUnitSelected();
+    return;
+  }
+
+  if ((endUnit = targetUnit.value) === '')
+  {
+    ErrorNoTargetUnitSelected();
+    return;
+  }
+
+  if (startingUnit === endUnit)
+  {
+    outputText.textContent = numToBeConverted;
+    return;
+  } else
+  {
+    const result = numToBeConverted * conversionSheet[startingUnit][endUnit];
+    outputText.textContent = result;
+  }
+});
